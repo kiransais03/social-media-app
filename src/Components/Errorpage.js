11da750ecee900d398154a9443fdc6842cdce141
kiransai <kiransais03@gmail.com>
2023-08-05 19:03:37 +0530

    p2

--- src/Components/Errorpage.js
+++ src/Components/Errorpage.js
@@ -2,6 +2,7 @@ import React from "react";
 import img404 from "../Images/404-tab-cartoon_114341-28.jpg";
 import "../css/Errorpage.css";
 import { Helmet } from "react-helmet-async";
+import bootstrap from 'bootstrap'
 
 
 let Errorpage = ()=>{
@@ -12,7 +13,7 @@ let Errorpage = ()=>{
         </Helmet>
 
         <div className="errordiv">
-           <img className="errorimg" src={img404} alt="404 Error,Page Not Found"/>
+           <img className="errorimg img-fluid" src={img404} alt="404 Error,Page Not Found"/>
        </div>
        </div>
     )
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+let Navbar = ()=>{
+    return (
+        <>
+        <h1>TravelMedia.in</h1>
+        </>
+    )
+}
+
+
+export default Navbar;

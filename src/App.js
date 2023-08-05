abf275faa2eee1bf655fd0d9660d62792a54d503
kiransai <kiransais03@gmail.com>
2023-08-05 22:25:07 +0530

    p4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import {Routes,Route} from "react-router-dom";
+
+//import components
 import Home from "./Components/Home";
 import Details from "./Components/Details";
 import Account from "./Components/Account";
 import Notifications from "./Components/Notifications";
-import {Routes,Route} from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import Errorpage from "./Components/Errorpage";
 
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/item" element={<Details />}>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
+import home from "../Images/home.png"
+import notifications from "../Images/notifications.png";
+import account from "../Images/account.png";
+import details from "../Images/details.png";
+import "../css/Navbar.css"
 
 let Navbar = ()=>{
+
+useEffect(()=>{
+    const images = document.querySelectorAll('.nav-centrediv img');
+
+    images.forEach(image => {image.className='my-2 mx-4';}); },[])
+
     return (
         <>
-        <h1>TravelMedia.in</h1>
+        <div className="navbardiv">
+          <h4 className="ms-3">TravelMedia.in</h4>
+            <div className="nav-centrediv">
+            <NavLink to="/"> <img src={home} alt="home"/> </NavLink> 
+            <NavLink to="/notifications">   <img src={notifications} alt="notifications"/> </NavLink> 
+            <NavLink to="/details">    <img src={details} alt="details"/> </NavLink> 
+            <NavLink to="/account">   <img src={account} alt="account"/> </NavLink> 
+             </div>
+             <h1 style={{visibility:"hidden"}}>TravelMedia.in</h1>
+        </div>
         </>
     )
 }
